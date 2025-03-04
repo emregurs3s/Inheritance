909711591a301e62c2c85097fe23919edcc135cf
Emre Gürses <106022391+emregurs3s@users.noreply.github.com>
2025-03-04 14:15:37 +0300

    Add files via upload

---
+++ Inheritance/Inheritance/Inheritance/Classes/Araba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.Classes
+{
+    internal class Araba
+    {
+       public string vites;
+       public string marka;
+        public void Yazdir(string marka, string vites)
+        {
+            Console.WriteLine( marka +" "+ vites);
+        }
+    }
+
+    
+
+}
+
---
+++ Inheritance/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using Inheritance.Classes;
+
+BMW bmw = new BMW();
+
+bmw.marka = "BMW";
+bmw.vites = "Düz vites";
+
+bmw.Yazdir(bmw.marka, bmw.vites);
+
+Porsche pr = new Porsche();
+pr.marka = "Porsche";
+pr.vites = "Otomatik vites";
+
+pr.Yazdir(pr.marka, pr.vites);
+
+Mercedes mr = new Mercedes();
+mr.marka = "Mercedes";
+mr.vites = "Otomatik vites";
+mr.Yazdir(mr.marka, mr.vites);
+
+Audi au = new Audi();
+au.marka = "Audi";
+au.vites = "Otomatik vites";
+au.Yazdir(au.marka, au.vites);
+
+Toyota ty = new Toyota();
+
+ty.marka = "Toyota";
+ty.vites = "Düz vites";
+ty.Yazdir(ty.marka, ty.vites);
+
+Togg togg = new Togg();
+togg.marka = "Togg";
+togg.vites = "Otomatik vites";
+togg.Yazdir(togg.marka, togg.vites);
+
